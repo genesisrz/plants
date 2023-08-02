918464f6440672a86835c2ad2940c181e590469b
Genesis Roa <groazambrano@deloitte.es>
2023-08-02 12:58:46 +0200

    first commit

---
+++ .sfdx/typings/lwc/depositphotos_81595024stockillustra.asset.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/depositphotos_81595024stockillustra" {
+    var depositphotos_81595024stockillustra: string;
+    export default depositphotos_81595024stockillustra;
+}
